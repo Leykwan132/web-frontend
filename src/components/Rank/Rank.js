1dce523440cb0ae2ddb963740f8b7b4ebc3ac89f
unknown <you@example.com>
2020-03-29 16:01:51 +0800

    op

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -3,7 +3,7 @@ import React from "react";
 const Rank = ({ name, entries }) => {
   return (
     <div style={{ marginTop: "6rem" }}>
-      <div className="white f3">{`${name}, your current rank is ... `}</div>
+      <div className="white f3">{`${name}, your current entry count is ... `}</div>
       <div className="white f1">{entries}</div>
     </div>
   );
